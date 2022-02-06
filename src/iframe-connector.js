bb3e3bc283bb62bdffb95a8db01437c2efac3f54
Aleksey Bykhun <theisease@gmail.com>
2022-02-07 04:10:32 +0800

    add event emitter

--- src/iframe-connector.js
+++ src/iframe-connector.js
@@ -1,21 +1,45 @@
+import { parseCommand } from "./state";
+
 export const game = document.querySelector('iframe#game');
 
-console.log('game', game);
+export const gameWindow = game.contentWindow;
 
-export const emitter = new EventTarget();
+export const parseMessage = (message) => {
+    // get roomid
+    let [roomid, ...commands] = message.split('\n');
+
+    if (roomid.charAt(0) !== '>') {
+        commands = [roomid, ...commands];
+        roomid = '';
+    } else {
+        roomid = roomid.substr(1);
+    }
+
+    if (!roomid.includes('battle')) {
+        console.log('ignoring roomid', roomid);
+        return;
+    }
+
+    commands.forEach(parseCommand);
+
+}
+
 
 game.addEventListener('load', () => {
     console.log('game loaded');
-    const gameWindow = game.contentWindow;
 
-    gameWindow.postMessage("TEST", "*")
+    gameWindow.postMessage({ for: 'showdown-client', data: "ping" }, "*");
 
     window.addEventListener('message', (event) => {
-        console.log('WebXR: receive game message', event.origin, event.data);
+        console.log('WebXR: receive game message', event.data);
 
-        if (!event.origin.includes('localhost')) return;
+        const { data, dest } = event.data;
 
-        // emitter.dispatchEvent(event);
+        if (dest !== 'webxr') return;
+
+        console.log('WebXR: event filtered', data);
+
+        parseMessage(data);
 
     }, false);
 });
